287700afc3cc1af76db15d02d15dcd3f1294d15b
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-05-13 03:50:30 +0900

    リポジトリ作成者のモデルを作成

---
+++ lib/model/repository_owner.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'repository_owner.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RepositoryOwner _$RepositoryOwnerFromJson(Map<String, dynamic> json) =>
+    RepositoryOwner(
+      avatarUrl: json['avatar_url'] as String,
+    );
+
+Map<String, dynamic> _$RepositoryOwnerToJson(RepositoryOwner instance) =>
+    <String, dynamic>{
+      'avatar_url': instance.avatarUrl,
+    };
