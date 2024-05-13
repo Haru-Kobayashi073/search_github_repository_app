e1760a6c1f8ab4b8b8394720df31d1561f0fd8fc
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-05-14 02:02:04 +0900

    アイコンを扱いやすい状態に生成

--- lib/utils/gen/assets.gen.dart
+++ lib/utils/gen/assets.gen.dart
@@ -7,6 +7,19 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/git-fork.svg
+  String get gitFork => 'assets/icons/git-fork.svg';
+
+  /// File path: assets/icons/issue-opened.svg
+  String get issueOpened => 'assets/icons/issue-opened.svg';
+
+  /// List of all assets
+  List<String> get values => [gitFork, issueOpened];
+}
+
 class $AssetsImagesGen {
   const $AssetsImagesGen();
 
@@ -26,5 +39,6 @@ class $AssetsImagesGen {
 class Assets {
   Assets._();
 
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
   static const $AssetsImagesGen images = $AssetsImagesGen();
 }
