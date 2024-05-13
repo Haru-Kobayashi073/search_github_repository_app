ef8b355fcd43323b149b3db7841f9ad15adf390f
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-05-13 17:56:34 +0900

    縦画面で固定

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:search_github_repository_app/presentation/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(
     const ProviderScope(
       child: App(),
