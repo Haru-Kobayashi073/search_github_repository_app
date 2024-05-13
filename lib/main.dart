2fe31f94d96a67bef70895fe3c5956f97ea14aa7
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-05-13 17:56:14 +0900

    MaterialAppを持ったクラスを切り出す

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:search_github_repository_app/presentation/search_repository/search_repository_page.dart';
+import 'package:search_github_repository_app/presentation/app.dart';
 
 void main() {
   runApp(
     const ProviderScope(
-      child: MainApp(),
+      child: App(),
     ),
   );
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SearchRepositoryPage(),
-    );
-  }
 }
