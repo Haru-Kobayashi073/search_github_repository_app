24142fffa2f8230b27d2ca93afda1e1f4e7050d5
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-05-14 19:55:51 +0900

    言語切り替え用のボタンを実装

--- lib/presentation/components/app_drawer.dart
+++ lib/presentation/components/app_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:search_github_repository_app/i18n/strings.g.dart';
+import 'package:search_github_repository_app/utils/locale_service.dart';
 import 'package:search_github_repository_app/utils/theme_service.dart';
 
 class AppDrawer extends ConsumerWidget {
@@ -7,11 +9,13 @@ class AppDrawer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final t = Translations.of(context);
+
     return Drawer(
       child: ListView(
         children: [
           ExpansionTile(
-            title: const Text('テーマ'),
+            title: Text(t.drawer.theme.title),
             childrenPadding: const EdgeInsets.only(left: 40),
             shape: const Border(),
             leading: switch (ref.watch(themeServiceProvider)) {
@@ -20,17 +24,56 @@ class AppDrawer extends ConsumerWidget {
                 const Icon(Icons.light_mode),
               ThemeMode.dark => const Icon(Icons.dark_mode),
             },
-            children: ThemeInformation.values
-                .map(
-                  (theme) => RadioListTile(
-                    title: Text(theme.title),
-                    groupValue: ref.watch(themeServiceProvider),
-                    value: theme.themeMode,
-                    onChanged: (value) =>
-                        ref.read(themeServiceProvider.notifier).toggle(theme),
-                  ),
-                )
-                .toList(),
+            children: [
+              RadioListTile(
+                title: Text(t.drawer.theme.system),
+                groupValue: ref.watch(themeServiceProvider),
+                value: ThemeMode.system,
+                onChanged: (value) => ref
+                    .read(themeServiceProvider.notifier)
+                    .toggle(ThemeMode.system),
+              ),
+              RadioListTile(
+                title: Text(t.drawer.theme.light),
+                groupValue: ref.watch(themeServiceProvider),
+                value: ThemeMode.light,
+                onChanged: (value) => ref
+                    .read(themeServiceProvider.notifier)
+                    .toggle(ThemeMode.light),
+              ),
+              RadioListTile(
+                title: Text(t.drawer.theme.dark),
+                groupValue: ref.watch(themeServiceProvider),
+                value: ThemeMode.dark,
+                onChanged: (value) => ref
+                    .read(themeServiceProvider.notifier)
+                    .toggle(ThemeMode.dark),
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: Text(t.drawer.language.title),
+            childrenPadding: const EdgeInsets.only(left: 40),
+            shape: const Border(),
+            leading: const Icon(Icons.language),
+            children: [
+              RadioListTile(
+                title: Text(t.drawer.language.ja),
+                groupValue: ref.watch(localeServiceProvider),
+                value: AppLocale.ja,
+                onChanged: (value) => ref
+                    .read(localeServiceProvider.notifier)
+                    .changeLocale(AppLocale.ja),
+              ),
+              RadioListTile(
+                title: Text(t.drawer.language.en),
+                groupValue: ref.watch(localeServiceProvider),
+                value: AppLocale.en,
+                onChanged: (value) => ref
+                    .read(localeServiceProvider.notifier)
+                    .changeLocale(AppLocale.en),
+              ),
+            ],
           ),
         ],
       ),
