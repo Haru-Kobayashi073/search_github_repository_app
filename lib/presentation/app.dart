6cdb1cf3eb4d941b54b185482721e9efd2f6cdd0
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-05-14 19:53:52 +0900

    言語設定を購読

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:search_github_repository_app/i18n/strings.g.dart';
 import 'package:search_github_repository_app/presentation/search_repository/search_repository_page.dart';
+import 'package:search_github_repository_app/utils/locale_service.dart';
 import 'package:search_github_repository_app/utils/theme_service.dart';
 
 class App extends ConsumerWidget {
@@ -8,10 +11,15 @@ class App extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final currentLocale = ref.watch(localeServiceProvider);
+
     return MaterialApp(
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       themeMode: ref.watch(themeServiceProvider),
+      locale: currentLocale.flutterLocale,
+      supportedLocales: AppLocaleUtils.supportedLocales,
+      localizationsDelegates: GlobalMaterialLocalizations.delegates,
       home: const SearchRepositoryPage(),
     );
   }
